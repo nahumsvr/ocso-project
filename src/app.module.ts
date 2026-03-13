27c1749234788f76f71c1556d3a926939b7f4399
nahumsvr <nahumnea@gmail.com>
2026-03-13 10:40:45 -0600

    Se integró el ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,34 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { EmployeesModule } from "./employees/employees.module";
-import { ProductsModule } from './products/products.module';
+import { ProductsModule } from "./products/products.module";
 
 @Module({
-  imports: [EmployeesModule, ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Lo hace disponible en toda la app sin re-importar
+    }),
+    // 2. Usamos forRootAsync para inyectar el ConfigService
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get<string>("DB_HOST"),
+        port: configService.get<number>("DB_PORT", 5432),
+        username: configService.get<string>("DB_USERNAME", "postgres"),
+        password: configService.get<string>("DB_PASSWORD"),
+        database: configService.get<string>("DB_NAME"),
+        entities: [__dirname + "/**/*.entity{.ts,.js}"],
+        synchronize: true, // Cuidado: Solo para desarrollo
+      }),
+    }),
+    EmployeesModule,
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
